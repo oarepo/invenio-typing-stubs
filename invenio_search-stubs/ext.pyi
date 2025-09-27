de2f6a1030da86f59d9fc26293573b1897074bc8
Mirek Simek <miroslav.simek@gmail.com>
2025-09-27 07:52:04 +0200

    fix: invenio-search

--- invenio_search-stubs/ext.pyi
+++ invenio_search-stubs/ext.pyi
@@ -6,15 +6,49 @@ class _SearchState:
     app: Flask
     mappings: dict[str, str]
     aliases: dict[str, dict[str, str]]
-    templates: dict[str, str]
-    component_templates: dict[str, str]
-    index_templates: dict[str, str]
     current_suffix: str
-    client: Any
     cluster_version: list[int]
     cluster_distribution: str
     active_aliases: dict[str, dict[str, str]]
+    entry_point_group_templates: str | None
+    entry_point_group_component_templates: str | None
+    entry_point_group_index_templates: str | None
+    _client: Any | None
+    _current_suffix: str | None
 
+    def __init__(
+        self,
+        app: Flask,
+        entry_point_group_mappings: str | None = ...,
+        entry_point_group_templates: str | None = ...,
+        entry_point_group_component_templates: str | None = ...,
+        entry_point_group_index_templates: str | None = ...,
+        **kwargs: Any,
+    ) -> None: ...
+    @property
+    def templates(self) -> dict[str, str]: ...
+    @property
+    def component_templates(self) -> dict[str, str]: ...
+    @property
+    def index_templates(self) -> dict[str, str]: ...
+    @property
+    def client(self) -> Any: ...
+    @staticmethod
+    def _get_mappings_module(module: str) -> str: ...
+    def _collect_templates(self, entrypoint_group: str) -> dict[str, str]: ...
+    def _client_builder(self) -> Any: ...
+    def _get_indices(self, tree_or_filename: dict[str, Any]) -> Iterable[str]: ...
+    def _replace_prefix(
+        self, template_path: str, body: str, enforce_prefix: bool
+    ) -> str: ...
+    def _put_template(
+        self,
+        template_name: str,
+        template_file: str,
+        put_function: Any,
+        ignore: list[int] | None,
+        enforce_prefix: bool = ...,
+    ) -> tuple[str, Any]: ...
     def register_mappings(self, alias: str, package_name: str) -> None: ...
     def register_templates(self, module: str) -> dict[str, str]: ...
     def load_entry_point_group_mappings(
@@ -53,6 +87,7 @@ class _SearchState:
 
 class InvenioSearch:
     _state: _SearchState
+    _clients: dict[str, Any]
 
     def __init__(self, app: Flask | None = None, **kwargs: Any) -> None: ...
     def init_app(
--- invenio_search-stubs/proxies.pyi
+++ invenio_search-stubs/proxies.pyi
@@ -1,5 +1,10 @@
 from typing import Any
+
+from invenio_search.ext import _SearchState
 from werkzeug.local import LocalProxy
 
-current_search: LocalProxy[Any]
+def _get_current_search() -> _SearchState: ...
+def _get_current_search_client() -> Any: ...
+
+current_search: LocalProxy[_SearchState]
 current_search_client: LocalProxy[Any]
