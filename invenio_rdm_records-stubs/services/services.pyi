ec183d2db22b8d5cda42bf08d40bc9025c485919
Mirek Simek <miroslav.simek@gmail.com>
2025-10-01 08:31:58 +0200

    Better typing for rdm service

--- invenio_rdm_records-stubs/services/services.pyi
+++ invenio_rdm_records-stubs/services/services.pyi
@@ -2,6 +2,7 @@ from __future__ import annotations
 
 from typing import Any, Generic, Optional, TypeVar
 
+from flask_principal import Identity
 from invenio_drafts_resources.services.records.service import RecordService
 from invenio_rdm_records.services.config import RDMRecordServiceConfig
 from invenio_records_resources.services.records.schema import ServiceSchemaWrapper
@@ -36,30 +37,38 @@ class RDMRecordService(RecordService[C], Generic[C]):
     def schema_quota(self) -> ServiceSchemaWrapper: ...
 
     # Service methods
-    def lift_embargo(self, identity, _id, uow=None): ...
+    def lift_embargo(self, identity: Identity, _id: str, uow=None): ...
     def scan_expired_embargos(self, identity): ...
-    def oai_result_item(self, identity, oai_record_source): ...
+    def oai_result_item(self, identity: Identity, oai_record_source): ...
     def delete_record(
         self,
-        identity,
-        id_,
+        identity: Identity,
+        id_: str,
         data,
         expand: bool = ...,
         uow=None,
         revision_id: Optional[int] = ...,
     ): ...
-    def update_tombstone(self, identity, id_, data, expand: bool = ..., uow=None): ...
-    def cleanup_record(self, identity, id_, uow=None): ...
-    def restore_record(self, identity, id_, expand: bool = ..., uow=None): ...
-    def mark_record_for_purge(self, identity, id_, expand: bool = ..., uow=None): ...
-    def unmark_record_for_purge(self, identity, id_, expand: bool = ..., uow=None): ...
-    def purge_record(self, identity, id_, uow=None): ...
-    def publish(self, identity, id_, uow=None, expand: bool = ...): ...
+    def update_tombstone(
+        self, identity: Identity, id_: str, data, expand: bool = ..., uow=None
+    ): ...
+    def cleanup_record(self, identity: Identity, id_: str, uow=None): ...
+    def restore_record(
+        self, identity: Identity, id_: str, expand: bool = ..., uow=None
+    ): ...
+    def mark_record_for_purge(
+        self, identity: Identity, id_: str, expand: bool = ..., uow=None
+    ): ...
+    def unmark_record_for_purge(
+        self, identity: Identity, id_: str, expand: bool = ..., uow=None
+    ): ...
+    def purge_record(self, identity: Identity, id_: str, uow=None): ...
+    def publish(self, identity: Identity, id_: str, uow=None, expand: bool = ...): ...
 
     # Note: keep base search() signature to avoid incompatible override.
     def search_drafts(
         self,
-        identity,
+        identity: Identity,
         params: Optional[dict[str, Any]] = ...,
         search_preference: Optional[str] = ...,
         expand: bool = ...,
@@ -68,8 +77,8 @@ class RDMRecordService(RecordService[C], Generic[C]):
     ): ...
     def search_versions(
         self,
-        identity,
-        id_,
+        identity: Identity,
+        id_: str,
         params: Optional[dict[str, Any]] = ...,
         search_preference: Optional[str] = ...,
         expand: bool = ...,
@@ -78,21 +87,26 @@ class RDMRecordService(RecordService[C], Generic[C]):
     ): ...
     def scan_versions(
         self,
-        identity,
-        id_,
+        identity: Identity,
+        id_: str,
         params: Optional[dict[str, Any]] = ...,
         search_preference: Optional[str] = ...,
         expand: bool = ...,
         permission_action: str = ...,
         **kwargs,
     ): ...
-
-    # read() inherited
-    def read_draft(self, identity, id_, expand: bool = ...): ...
+    def read(  # type: ignore[override]
+        self,
+        identity: Identity,
+        id_: str,
+        expand: bool = False,
+        include_deleted: bool = False,
+    ): ...
+    def read_draft(self, identity: Identity, id_: str, expand: bool = ...): ...
     def update_draft(
         self,
-        identity,
-        id_,
+        identity: Identity,
+        id_: str,
         data,
         revision_id: Optional[int] = ...,
         uow=None,
@@ -100,14 +114,18 @@ class RDMRecordService(RecordService[C], Generic[C]):
     ): ...
     def set_quota(
         self,
-        identity,
-        id_,
+        identity: Identity,
+        id_: str,
         data,
         files_attr: str = ...,
         uow=None,
     ) -> bool: ...
-    def set_user_quota(self, identity, id_, data, uow=None) -> bool: ...
-    def search_revisions(self, identity, id_): ...
+    def set_user_quota(self, identity: Identity, id_: str, data, uow=None) -> bool: ...
+    def search_revisions(self, identity: Identity, id_): ...
     def read_revision(
-        self, identity, id_, revision_id, include_previous: bool = ...
+        self,
+        identity: Identity,
+        id_: str,
+        revision_id: str,
+        include_previous: bool = ...,
     ): ...
